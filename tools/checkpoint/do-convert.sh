bd1d6508a97735d804b8d2bbdcc0c6792f49a327
Alexander Hagele <alexanderhagele@gmail.com>
2025-03-17 10:08:33 +0100

    update conversion script with example for 8b

--- tools/checkpoint/do-convert.sh
+++ tools/checkpoint/do-convert.sh
@@ -5,19 +5,21 @@
 #   - core (torch backend) ---> HF    ,  always.
 
 
-MEGATRON_LM_DIR=/iopsstor/scratch/cscs/$USER/Megatron-LM
-CKPT_PATH=/iopsstor/scratch/cscs/schlag/experiments/merge-for-v2/Megatron-LM/logs/Meg-Runs/apertus2_baselines/apertus2-1b-21n-4096sl-504gbsz-ademamix-wsd-xielu-crossDocAttn-goldfish-beta2-qkNorm-untie/checkpoints
+MEGATRON_LM_DIR=$(dirname $(dirname $( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P ))) # Grandparent of current file location.
+CKPT_PATH=/iopsstor/scratch/cscs/schlag/main_run_megatron/Megatron-LM/logs/Meg-Runs/main-runs-v1/apertus3-8b-128-nodes/checkpoints/
 
+ITERATIONS=300000
+ITER_STRING=$(($ITERATIONS/1000))k
 # [torch_dist -> torch] dependencies
 CKPT_IS_TORCH_DIST=true
 TORCH_DIST_SCRIPT=$MEGATRON_LM_DIR/scripts/conversion/torchdist_2_torch.py
-TORCH_CKPT_SAVE_PATH=/iopsstor/scratch/cscs/$USER/Meg-Checkpoints/test2-apertus2-1b-21n
+TORCH_CKPT_SAVE_PATH=/iopsstor/scratch/cscs/$USER/Meg-Checkpoints/apertus3-8b-128n-$ITER_STRING
 # [core (torch) --> HF] dependencies
-HF_SAVE_DIR=/iopsstor/scratch/cscs/$USER/Meg-Checkpoints/hf-checkpoints
-SAVE_DIR=$HF_SAVE_DIR/test2-apertus2-1b-21n
+HF_SAVE_DIR=/capstor/store/cscs/swissai/a06/main_run_megatron/hf-checkpoints/
+SAVE_DIR=$HF_SAVE_DIR/apertus3-8b-128n-$ITER_STRING
 mkdir -p $HF_SAVE_DIR
 LOADER=core
-SAVER=swissai_hf
+SAVER=llama_hf
 
 
 # Run torch_dist --> torch
@@ -27,7 +29,8 @@ if [[ "$CKPT_IS_TORCH_DIST" == true ]]; then
     CUDA_DEVICE_MAX_CONNECTIONS=1 torchrun $TORCH_DIST_SCRIPT \
     --bf16 \
     --load $CKPT_PATH \
-    --ckpt-convert-save $TORCH_CKPT_SAVE_PATH
+    --ckpt-convert-save $TORCH_CKPT_SAVE_PATH \
+    --ckpt-step $ITERATIONS
 else
     LOAD_DIR=$CKPT_PATH
     echo "Skipping torch_dist --> torch conversion..."
@@ -42,4 +45,4 @@ python $MEGATRON_LM_DIR/tools/checkpoint/convert.py \
     --saver $SAVER \
     --load-dir $LOAD_DIR \
     --save-dir $SAVE_DIR \
-    #\ --hf-tokenizer .....
+    --hf-tokenizer alehc/swissai-tokenizer
