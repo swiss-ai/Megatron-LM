0d81c063f38b70a55a72557c1933bf2d7ea2b0a8
Alexander Hagele <alexanderhagele@gmail.com>
2025-03-20 09:17:44 +0100

    latest iters of main runs evals

--- scripts/evaluation/example_submissions/eval_apertus1b.sh
+++ scripts/evaluation/example_submissions/eval_apertus1b.sh
@@ -3,9 +3,16 @@ export LOGS_ROOT=$SCRATCH/eval-logs/main-v1
 # export MODEL=/capstor/store/cscs/swissai/a06/main_run_megatron/Megatron-LM/logs/Meg-Runs/main-runs-v1/apertus3-1b-21-nodes/checkpoints/
 export MODEL=/iopsstor/scratch/cscs/schlag/main_run_megatron/Megatron-LM/logs/Meg-Runs/main-runs-v1/apertus3-1b-21-nodes/checkpoints/
 export NAME=Apertus3-1.5B
-export ARGS="--convert-to-hf --size 1 --wandb-entity epflmlo-epfl --wandb-project swissai-eval-main-v1 --wandb-id $NAME --bs auto --tokens-per-iter 2064384 --tasks swissai_eval"
+export ARGS="--convert-to-hf --size 1 --wandb-entity epflmlo-epfl --wandb-project swissai-eval-main-v1.1 --wandb-id $NAME --bs auto --tokens-per-iter 2064384 --tasks swissai_eval"
 
 # those were in capstor at time of submitting
-# bash scripts/evaluation/submit_evaluation.sh $MODEL $ARGS --iterations "60000,140000,220000,300000,380000"
+# ITS="60000,140000,220000,300000,380000"
 # on iopsstor
-bash scripts/evaluation/submit_evaluation.sh $MODEL $ARGS --iterations "460000,510000"
+# ITS="460000,510000"
+# ITS="610000,710000,810000"
+# ITS="910000,1100000"
+# ITS="1200000,1300000"
+ITS="1400000,1500000"
+bash scripts/evaluation/submit_evaluation.sh $MODEL $ARGS --iterations $ITS
+
+
--- scripts/evaluation/example_submissions/eval_apertus8b.sh
+++ scripts/evaluation/example_submissions/eval_apertus8b.sh
@@ -3,7 +3,12 @@ export LOGS_ROOT=$SCRATCH/eval-logs/main-v1
 # export MODEL=/capstor/store/cscs/swissai/a06/main_run_megatron/Megatron-LM/logs/Meg-Runs/main-runs-v1/apertus3-8b-128-nodes/checkpoints/
 export MODEL=/iopsstor/scratch/cscs/schlag/main_run_megatron/Megatron-LM/logs/Meg-Runs/main-runs-v1/apertus3-8b-128-nodes/checkpoints/
 export NAME=Apertus3-8B
-export ARGS="--convert-to-hf --size 8 --wandb-entity epflmlo-epfl --wandb-project swissai-eval-main-v1 --wandb-id $NAME --bs auto --tokens-per-iter 4194304 --tasks swissai_eval"
+export ARGS="--convert-to-hf --size 8 --wandb-entity epflmlo-epfl --wandb-project swissai-eval-main-v1.1 --wandb-id $NAME --bs auto --tokens-per-iter 4194304 --tasks swissai_eval"
 
-ITS="162000,192000,222000,262000"
+# ITS="50000,100000"
+# ITS="162000,192000,222000,262000"
+# ITS="312000,362000,412000"
+# ITS="462000,512000,542000"
+# ITS="600000,668000,736000"
+ITS="804000,866000"
 bash scripts/evaluation/submit_evaluation.sh $MODEL $ARGS --iterations $ITS
