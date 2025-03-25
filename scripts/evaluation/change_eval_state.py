0983e5e15302736d03e7cf16b7958c38777f42cc
Andreas Marfurt <andreas.marfurt@hslu.ch>
2025-03-25 14:39:44 +0100

    Added script to manually change an evaluation's state in the metadata.

---
+++ scripts/evaluation/change_eval_state.py
@@ -0,0 +1,29 @@
+"""Manually changes an evaluation's state in the metadata (e.g. to rerun an evaluation)."""
+
+from check_evaluations import EvalMetadata, State
+
+
+def parse_args():
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Manually set an evaluation's state.")
+    parser.add_argument("--model_name", required=True)
+    parser.add_argument("--iteration", type=int, required=True)
+    parser.add_argument(
+        "--state", required=True, choices=[s.name.lower() for s in State]
+    )
+    return parser.parse_args()
+
+
+def main(args):
+    new_state = None
+    for state in State:
+        if state.name.lower() == args.state:
+            new_state = state
+            break
+    eval_metadata = EvalMetadata()
+    eval_metadata.update_iteration_metadata(args.model_name, args.iteration, new_state)
+
+
+if __name__ == "__main__":
+    main(parse_args())
