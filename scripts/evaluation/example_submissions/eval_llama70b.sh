092601a0f5e7b4a32f33b5419a2cb9a57dbc16d5
Andreas Marfurt <andreas.marfurt@hslu.ch>
2025-06-01 11:51:23 +0200

    Have to run 70B models with limit 1000 to avoid timeout.

--- scripts/evaluation/example_submissions/eval_llama70b.sh
+++ scripts/evaluation/example_submissions/eval_llama70b.sh
@@ -3,6 +3,6 @@ export LOGS_ROOT=$SCRATCH/eval-logs/main-v1
 export MODEL=meta-llama/Llama-3.1-70B
 export TOKENIZER=$MODEL
 export NAME=Llama3.1-70B
-export ARGS="--size 70 --wandb-entity andreas-marfurt --wandb-project swissai-eval-main-v1.2 --wandb-id $NAME --bs auto --consumed-tokens 15000000000000 --tasks swissai_eval"
+export ARGS="--size 70 --wandb-entity andreas-marfurt --wandb-project swissai-eval-main-v1.2 --wandb-id $NAME --bs auto --consumed-tokens 15000000000000 --tasks swissai_eval --limit 1000"
 
 bash scripts/evaluation/submit_evaluation.sh $MODEL $ARGS
--- scripts/evaluation/example_submissions/eval_qwen2.5-72b.sh
+++ scripts/evaluation/example_submissions/eval_qwen2.5-72b.sh
@@ -3,6 +3,6 @@ export LOGS_ROOT=$SCRATCH/eval-logs/main-v1
 export MODEL=Qwen/Qwen2.5-72B
 export TOKENIZER=$MODEL
 export NAME=Qwen2.5-72B
-export ARGS="--size 70 --wandb-entity andreas-marfurt --wandb-project swissai-eval-main-v1.2 --wandb-id $NAME --bs auto --consumed-tokens 18000000000000 --tasks swissai_eval"
+export ARGS="--size 70 --wandb-entity andreas-marfurt --wandb-project swissai-eval-main-v1.2 --wandb-id $NAME --bs auto --consumed-tokens 18000000000000 --tasks swissai_eval --limit 1000"
 
 bash scripts/evaluation/submit_evaluation.sh $MODEL $ARGS
