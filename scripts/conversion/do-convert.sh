ad4a72345252183796d7ea1dc1fc690883fa2a24
dhia680 <garbayad@gmail.com>
2025-06-26 18:01:11 +0200

    update do-convert.sh

--- scripts/conversion/do-convert.sh
+++ scripts/conversion/do-convert.sh
@@ -7,14 +7,12 @@
 
 MEGATRON_LM_DIR=/iopsstor/scratch/cscs/$USER/Megatron-LM
 CKPT_PATH=/iopsstor/scratch/cscs/schlag/main_run_megatron/Megatron-LM/logs/Meg-Runs/main-runs-v1/apertus3-1b-21-nodes/checkpoints/
-# CKPT_PATH=/capstor/scratch/cscs/asolergi/main_run_70B_megatron/Megatron-LM/logs/Meg-Runs/main-runs-v1/apertus3-70b-512-nodes-1e-5lr/checkpoints-512-noOverlap/
 export PYTHONPATH=$MEGATRON_LM_DIR:$PYTHONPATH
 
 # [torch_dist -> torch] dependencies
 CKPT_IS_TORCH_DIST=true
 TORCH_DIST_SCRIPT=$MEGATRON_LM_DIR/scripts/conversion/torchdist_2_torch.py
 TORCH_CKPT_SAVE_PATH=/iopsstor/scratch/cscs/$USER/Meg-Checkpoints/test-logits-ap3-1b-21n-it2040000
-# TORCH_CKPT_SAVE_PATH=/iopsstor/scratch/cscs/$USER/Meg-Checkpoints/test-logits-ap3-70b-512n-it630000
 # [core (torch) --> HF] dependencies
 HF_SAVE_DIR=/iopsstor/scratch/cscs/$USER/Meg-Checkpoints/hf-checkpoints
 SAVE_DIR=$HF_SAVE_DIR/test-logits-ap3-1b-21n-it2040000
@@ -33,8 +31,7 @@ if [[ "$CKPT_IS_TORCH_DIST" == true ]]; then
     --load $CKPT_PATH \
     --ckpt-convert-save $TORCH_CKPT_SAVE_PATH \
     --ckpt-step 670000 \
-    # --pipeline-model-parallel-size=4
-# --pipeline-model-parallel-size=4  #for the 70b only (same as !!!! --nproc-per-node=4 !!!! right after torchrun)
+# add --pipeline-model-parallel-size=4  # for the 70b only ; also add --nproc-per-node=4 right after torchrun)
 else
     LOAD_DIR=$CKPT_PATH
     echo "Skipping torch_dist --> torch conversion..."
