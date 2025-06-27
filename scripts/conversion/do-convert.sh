b216e68a0e04c5929aa5768bdb72f8909ae66511
dhia680 <garbayad@gmail.com>
2025-06-27 13:34:53 +0200

    revert do-convert.sh

--- scripts/conversion/do-convert.sh
+++ scripts/conversion/do-convert.sh
@@ -6,17 +6,17 @@
 
 
 MEGATRON_LM_DIR=/iopsstor/scratch/cscs/$USER/Megatron-LM
-CKPT_PATH=/iopsstor/scratch/cscs/schlag/main_run_megatron/Megatron-LM/logs/Meg-Runs/main-runs-v1/apertus3-1b-21-nodes/checkpoints/
+CKPT_PATH=/iopsstor/scratch/cscs/schlag/experiments/merge-for-v2/Megatron-LM/logs/Meg-Runs/apertus2_baselines/apertus2-1b-21n-4096sl-504gbsz-ademamix-wsd-xielu-crossDocAttn-goldfish-beta2-qkNorm-untie/checkpoints
+
 export PYTHONPATH=$MEGATRON_LM_DIR:$PYTHONPATH
 
 # [torch_dist -> torch] dependencies
 CKPT_IS_TORCH_DIST=true
 TORCH_DIST_SCRIPT=$MEGATRON_LM_DIR/scripts/conversion/torchdist_2_torch.py
-TORCH_CKPT_SAVE_PATH=/iopsstor/scratch/cscs/$USER/Meg-Checkpoints/test-logits-ap3-1b-21n-it2040000
+TORCH_CKPT_SAVE_PATH=/iopsstor/scratch/cscs/$USER/Meg-Checkpoints/test2-apertus2-1b-21n
 # [core (torch) --> HF] dependencies
 HF_SAVE_DIR=/iopsstor/scratch/cscs/$USER/Meg-Checkpoints/hf-checkpoints
-SAVE_DIR=$HF_SAVE_DIR/test-logits-ap3-1b-21n-it2040000
-# SAVE_DIR=$HF_SAVE_DIR/test-logits-ap3-70b-512n-it630000
+SAVE_DIR=$HF_SAVE_DIR/test2-apertus2-1b-21n
 mkdir -p $HF_SAVE_DIR
 LOADER=core
 SAVER=swissai_hf
@@ -29,9 +29,7 @@ if [[ "$CKPT_IS_TORCH_DIST" == true ]]; then
     CUDA_DEVICE_MAX_CONNECTIONS=1 torchrun $TORCH_DIST_SCRIPT \
     --bf16 \
     --load $CKPT_PATH \
-    --ckpt-convert-save $TORCH_CKPT_SAVE_PATH \
-    --ckpt-step 670000 \
-# add --pipeline-model-parallel-size=4  # for the 70b only ; also add --nproc-per-node=4 right after torchrun)
+    --ckpt-convert-save $TORCH_CKPT_SAVE_PATH
 else
     LOAD_DIR=$CKPT_PATH
     echo "Skipping torch_dist --> torch conversion..."
