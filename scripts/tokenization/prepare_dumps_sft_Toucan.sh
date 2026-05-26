7a6db7ed9bc10fd9ca422f291b153896936e682e
Daniel Tamayo Mela <danitamayo.adhoc@gmail.com>
2026-05-27 00:43:32 +0200

    doc: Add tokenization scripts SFT

---
+++ scripts/tokenization/prepare_dumps_sft_Toucan.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --job-name=toucan
+#SBATCH --output=scripts/tokenization/logs/tok_%j.out
+#SBATCH --error=scripts/tokenization/logs/tok_%j.err
+#SBATCH --partition=normal
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=450G
+#SBATCH --time=0:30:00
+#SBATCH --account=infra01
+#SBATCH --reservation=SD-69241-apertus-1-5-0
+
+# HF_PATH="/iopsstor/scratch/cscs/hyukhymenko/sft-1.1-mixes/v1p5-mix-v1-26-04-cleaned"
+HF_PATH="/capstor/store/cscs/swissai/infra01/tmp_data/Toucan-1.5M_filtered"
+DATA_BASE="/capstor/scratch/cscs/dtamayomela/tokenize_it_data/Toucan-1.5M_filtered/output.parquet"
+MEGATRON_PATH="/capstor/scratch/cscs/dtamayomela/megatron/pre-training/megatron_tok_or"
+OUTPUT_FOLDER="datasets/Toucan-1.5M_filtered"
+
+srun --environment=/capstor/scratch/cscs/dtamayomela/containers/data-pipeline.toml bash -c "\
+    export PYTHONPATH=${MEGATRON_PATH}
+    cd ${MEGATRON_PATH}
+    python scripts/tokenization/apply_chat_template.py \
+        --input $HF_PATH \
+        --output $DATA_BASE \
+        --tokenizer /capstor/scratch/cscs/dtamayomela/tokenizer_fixed \
+        --num-proc 15 \
+        --tasks-per-worker 16"
+        # --tokenizer swiss-ai/Apertus-8B-Instruct-2509
+        # --tokenizer /capstor/store/cscs/swissai/infra01/MLLM/tokenizer/apertus_emu3.5_wavtok_instruct_thinking_token_fixed \
+
+python3 scripts/tokenization/prepare_dumps.py \
+  --dataset-folder $(dirname $DATA_BASE) \
+  --preprocessing-metadata-folder "$OUTPUT_FOLDER" \
+  --n-dumps 1
