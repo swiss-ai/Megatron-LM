afdb599298dd56c9c4b26ff4ba776d0e795d9202
Guanshujie Fu <fuguan@student.ethz.ch>
2026-06-22 10:06:00 +0200

    fix OffloadingFP8Config init in training.py

--- megatron/training/training.py
+++ megatron/training/training.py
@@ -1849,7 +1849,7 @@ def train_step(forward_step_func, data_iterator, model, optimizer, opt_param_sch
             and args.moe_use_inplace_fp8_param
             and not config.moe_offloading_experts_debug_mode
         ):
-            FP8ExpertsParameterManager.create_instance(config)
+            FP8ExpertsParameterManager.create_instance(OffloadingFP8Config.from_transformer_config(config))
             FP8ExpertsParameterManager.mark_first_microbatch()
 
         if (
