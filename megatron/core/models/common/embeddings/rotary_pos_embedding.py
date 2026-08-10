fix: set rope scaling factor default to 1 and fix factor call in gpt_builders

megatron/core/models/common/embeddings/rotary_pos_embedding.py
@@ -63,7 +63,7 @@ class RotaryEmbedding(nn.Module):
         seq_len_interpolation_factor: float = None,
         rotary_base: int = 10000,
         rope_scaling: bool = False,
-        rope_scaling_factor: float = 8.0,
+        rope_scaling_factor: float = 1.0,
         use_cpu_initialization: bool = False,
         cp_group: Optional[torch.distributed.ProcessGroup] = None,
     ) -> None:
@@ -92,7 +92,7 @@ class RotaryEmbedding(nn.Module):
     def _apply_scaling(
         self,
         freqs,
-        factor=8,
+        factor=1,
         low_freq_factor=1,
         high_freq_factor=4,
         original_max_position_embeddings=8192,